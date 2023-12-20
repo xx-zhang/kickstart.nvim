0513b4fdd9278405bc191aebaf91f8fa0f7a2b43
Your Name <you@example.com>
2023-12-20 15:46:39 +0800

    -

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -16,23 +16,27 @@ require('lazy').setup({
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
+  -- {
+  --   -- LSP Configuration & Plugins
+  --   'neovim/nvim-lspconfig',
+  --   dependencies = {
+  --     -- Automatically install LSPs to stdpath for neovim
+  --     'williamboman/mason.nvim',
+  --     'williamboman/mason-lspconfig.nvim',
+
+  --     -- Useful status updates for LSP
+  --     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+  --     { 'j-hui/fidget.nvim', opts = {} },
+
+  --     -- Additional lua configuration, makes nvim stuff amazing!
+  --     'folke/neodev.nvim',
+  --   },
+  -- },
+  -- install coc.vim replace mason 
   {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
+    'neoclide/coc.nvim',
+    branch = 'release'
   },
-
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -7,40 +7,133 @@ local on_attach = function(_, bufnr)
   --
   -- In this case, we create a function that lets us more easily define mappings specific
   -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+  -- local nmap = function(keys, func, desc)
+  --   if desc then
+  --     desc = 'LSP: ' .. desc
+  --   end
+
+  --   vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  -- end
+
+  
+  -----------map info  --------------------------------
+    -- +================================== coc.nvim  ======================================+ --
+    -- Some servers have issues with backup files, see #649.
+    vim.opt.backup = false
+    vim.opt.writebackup = false
+    -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+    -- delays and poor user experience.
+    vim.opt.updatetime = 300
+    -- Always show the signcolumn, otherwise it would shift the text each time
+    -- diagnostics appear/become resolved.
+    vim.opt.signcolumn = 'yes'
+    -- Use tab for trigger completion with characters ahead and navigate.
+    -- NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+    -- other plugin before putting this into your config.
+    vim.api.nvim_set_keymap('i', '<TAB>', 'pumvisible() ? "\\<C-n>" : v:lua.CheckBackspace() ? "\\<TAB>" : coc#refresh()', {expr = true, silent = true})
+    vim.api.nvim_set_keymap('i', '<S-TAB>', 'pumvisible() ? "\\<C-p>" : "\\<C-h>"', {expr = true, silent = true})
+    -- Make <CR> to accept selected completion item or notify coc.nvim to format
+    -- <C-g>u breaks current undo, please make your own choice.
+    vim.api.nvim_set_keymap('i', '<CR>', 'pumvisible() ? coc#pum#confirm() : "\\<C-g>u\\<CR>\\<c-r>=coc#on_enter()\\<CR>"', {expr = true, silent = true})
+
+    function CheckBackspace()
+      local col = vim.fn.col('.') - 1
+      return (col == 0 or vim.fn.getline('.'):sub(col, col):match('%s'))
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    -- Use <c-space> to trigger completion.
+    if vim.fn.has('nvim') then
+      vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', {expr = true, silent = true})
+    else
+      vim.api.nvim_set_keymap('i', '<c-@>', 'coc#refresh()', {expr = true, silent = true})
+    end
+    -- Use `[g` and `]g` to navigate diagnostics
+    -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
+    vim.api.nvim_set_keymap('n', '[g', '<Plug>(coc-diagnostic-prev)', {silent = true})
+    vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-diagnostic-next)', {silent = true})
+    -- GoTo code navigation.
+    vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
+    vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
+    vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {silent = true})
+    vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
+    -- Use K to show documentation in preview window.
+    vim.api.nvim_set_keymap('n', 'K', ':call ShowDocumentation()<CR>', {silent = true})
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    function ShowDocumentation()
+      if vim.fn.CocAction('hasProvider', 'hover') then
+        vim.fn.CocActionAsync('doHover')
+      else
+        vim.fn.feedkeys('K', 'in')
+      end
+    end
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+    -- Highlight the symbol and its references when holding the cursor.
+    vim.cmd('autocmd CursorHold * silent call CocActionAsync("highlight")')
+    -- Symbol renaming.
+    vim.api.nvim_set_keymap('n', '<leader>rn', '<Plug>(coc-rename)', {silent = true})
+    -- Formatting selected code.
+    vim.api.nvim_set_keymap('x', '<leader>f', '<Plug>(coc-format-selected)', {silent = true})
+    vim.api.nvim_set_keymap('n', '<leader>f', '<Plug>(coc-format-selected)', {silent = true})
+    vim.cmd([[
+    augroup mygroup
+      autocmd!
+      " Setup formatexpr specified filetype(s).
+      autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+      " Update signature help on jump placeholder.
+      autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+    augroup end
+    ]])
+    -- Applying codeAction to the selected region.
+    -- Example: `<leader>aap` for current paragraph
+    vim.api.nvim_set_keymap('x', '<leader>a', '<Plug>(coc-codeaction-selected)', {silent = true})
+    vim.api.nvim_set_keymap('n', '<leader>a', '<Plug>(coc-codeaction-selected)', {silent = true})
+    -- Remap keys for applying codeAction to the current buffer.
+    vim.api.nvim_set_keymap('n', '<leader>ac', '<Plug>(coc-codeaction)', {silent = true})
+    -- Apply AutoFix to problem on the current line.
+    vim.api.nvim_set_keymap('n', '<leader>qf', '<Plug>(coc-fix-current)', {silent = true})
+    -- Run the Code Lens action on the current line.
+    vim.api.nvim_set_keymap('n', '<leader>cl', '<Plug>(coc-codelens-action)', {silent = true})
+    -- Map function and class text objects
+    -- NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+    vim.api.nvim_set_keymap('x', 'if', '<Plug>(coc-funcobj-i)', {silent = true})
+    vim.api.nvim_set_keymap('o', 'if', '<Plug>(coc-funcobj-i)', {silent = true})
+    vim.api.nvim_set_keymap('x', 'af', '<Plug>(coc-funcobj-a)', {silent = true})
+    vim.api.nvim_set_keymap('o', 'af', '<Plug>(coc-funcobj-a)', {silent = true})
+    vim.api.nvim_set_keymap('x', 'ic', '<Plug>(coc-classobj-i)', {silent = true})
+    vim.api.nvim_set_keymap('o', 'ic', '<Plug>(coc-classobj-i)', {silent = true})
+    vim.api.nvim_set_keymap('x', 'ac', '<Plug>(coc-classobj-a)', {silent = true})
+    vim.api.nvim_set_keymap('o', 'ac', '<Plug>(coc-classobj-a)', {silent = true})
+    -- Remap <C-f> and <C-b> for scroll float windows/popups.
+    if vim.fn.has('nvim-0.4.0') or vim.fn.has('patch-8.2.0750') then
+      vim.api.nvim_set_keymap('n', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "\\<C-f>"', {expr = true, silent = true, nowait = true})
+      vim.api.nvim_set_keymap('n', '<C-b>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "\\<C-b>"', {expr = true, silent = true, nowait = true})
+      vim.api.nvim_set_keymap('i', '<C-f>', 'coc#float#has_scroll() ? "\\<c-r>=coc#float#scroll(1)\\<cr>" : "\\<Right>"', {expr = true, silent = true, nowait = true})
+      vim.api.nvim_set_keymap('i', '<C-b>', 'coc#float#has_scroll() ? "\\<c-r>=coc#float#scroll(0)\\<cr>" : "\\<Left>"', {expr = true, silent = true, nowait = true})
+      vim.api.nvim_set_keymap('v', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "\\<C-f>"', {expr = true, silent = true, nowait = true})
+      vim.api.nvim_set_keymap('v', '<C-b>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "\\<C-b>"', {expr = true, silent = true, nowait = true})
+    end
+    -- Use CTRL-S for selections ranges.
+    -- Requires 'textDocument/selectionRange' support of language server.
+    vim.api.nvim_set_keymap('n', '<C-s>', '<Plug>(coc-range-select)', {silent = true})
+    vim.api.nvim_set_keymap('x', '<C-s>', '<Plug>(coc-range-select)', {silent = true})
+    -- Add `:Format` command to format current buffer.
+    vim.cmd('command! -nargs=0 Format :call CocActionAsync("format")')
+    -- Add `:Fold` command to fold current buffer.
+    vim.cmd('command! -nargs=? Fold :call CocAction("fold", <f-args>)')
+    -- Add `:OR` command for organize imports of the current buffer.
+    vim.cmd('command! -nargs=0 OR :call CocActionAsync("runCommand", "editor.action.organizeImport")')
+    -- Add (Neo)Vim's native statusline support.
+    -- NOTE: Please see `:h coc-status` for integrations with external plugins that
+    -- provide custom statusline: lightline.vim, vim-airline.
+    vim.opt.statusline = '%{coc#status()}%{get(b:,"coc_current_function","")}'
+--
+  -------------------tag -------------------------------
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  -- vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --   vim.lsp.buf.format()
+  -- end, { desc = 'Format current buffer with LSP' })
+  
 end
 
 -- document existing key chains
@@ -61,10 +154,13 @@ require('which-key').register({
   ['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
+-- cocvim replace mason 20231102 
+
+
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
-require('mason').setup()
-require('mason-lspconfig').setup()
+-- require('mason').setup()
+-- require('mason-lspconfig').setup()
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -92,29 +188,4 @@ local servers = {
   },
 }
 
--- Setup neovim lua configuration
-require('neodev').setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
-
--- vim: ts=2 sts=2 sw=2 et
